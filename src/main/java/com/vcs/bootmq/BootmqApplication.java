648f804b77bb1981be5ef91cf3871726d5c9ffc3
Vivek Chandra Sardar <chandravivek52@gmail.com>
2024-01-19 12:12:46 +0530

    initial-commit: set-up queue, exchange, routing-key, producer and consumer in RabbitMQ with spring-boot ...

---
+++ src/main/java/com/vcs/bootmq/BootmqApplication.java
@@ -0,0 +1,13 @@
+package com.vcs.bootmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootmqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootmqApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vcs/bootmq/BootmqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vcs.bootmq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootmqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
